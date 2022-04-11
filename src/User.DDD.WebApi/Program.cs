396f6f1867f470edd0344f91187ce9323220819f
dong <44517714+lurudong@users.noreply.github.com>
2022-04-11 22:42:26 +0800

    修改Dockerfile文件

--- src/User.DDD.WebApi/Program.cs
+++ src/User.DDD.WebApi/Program.cs
@@ -29,11 +29,8 @@ builder.Services.AddScoped<ILoginContract, LoginService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
